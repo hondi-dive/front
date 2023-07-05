8547e870ca5f8cd9386ad98fb9a761973bdb5f90
star <chlvkfksquf@gmail.com>
2023-07-06 02:40:51 +0900

    Add axios config

---
+++ src/apis/index.js
@@ -0,0 +1,29 @@
+import Axios from 'axios';
+
+export const createApi = () => {
+  const _customAxios = Axios.create({
+    baseURL: '172.30.1.54',
+    withCredentials: true,
+    validateStatus: (status) => status >= 200 && status < 400,
+  });
+
+  _customAxios.interceptors.response.use(
+    (response) => {
+      return Promise.resolve(response.data);
+    },
+
+    async (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  _customAxios.interceptors.request.use((config) => {
+    return config;
+  });
+
+  return _customAxios;
+};
+
+const customAxios = createApi();
+
+export default customAxios;
