3b2eaef46cfe07d719cd5e50d743a91d37c4f90e
star <chlvkfksquf@gmail.com>
2023-07-06 01:37:04 +0900

    피드 임시

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,68 @@
 const Feed = () => {
-  return <div>피드페이지</div>;
+  return (
+    <div>
+      <div>에서 이렇게 놀고있어요</div>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gridTemplateRows: '1fr 1fr',
+          rowGap: 10,
+          columnGap: 10,
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'blue',
+            width: 20,
+            height: 20,
+          }}
+        />
+        <div
+          style={{
+            backgroundColor: 'green',
+            width: 50,
+            height: 50,
+          }}
+        />
+        <div
+          style={{
+            backgroundColor: 'blue',
+            width: 20,
+            height: 20,
+          }}
+        />
+        <div
+          style={{
+            backgroundColor: 'green',
+            width: 20,
+            height: 20,
+          }}
+        />
+        <div
+          style={{
+            backgroundColor: 'blue',
+            width: 20,
+            height: 20,
+          }}
+        />
+        <div
+          style={{
+            backgroundColor: 'green',
+            width: 50,
+            height: 50,
+          }}
+        />
+        <div
+          style={{
+            backgroundColor: 'blue',
+            width: 20,
+            height: 20,
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
