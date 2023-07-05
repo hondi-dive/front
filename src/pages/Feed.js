fa173ac6158fe0c623e563fb0ceb58d8d502c086
star <chlvkfksquf@gmail.com>
2023-07-06 01:28:08 +0900

    Add feed page

---
+++ src/pages/Feed.js
@@ -0,0 +1,5 @@
+const Feed = () => {
+  return <div>피드페이지</div>;
+};
+
+export default Feed;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import Main from 'pages/Main';
 import Main2 from 'pages/Main2';
+import Feed from 'pages/Feed';
 
 const Routes2 = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/main2" element={<Main2 />} />
+      <Route path="/feed" element={<Feed />} />
     </Routes>
   );
 };
