9d48bf8e42f7336b2ff4fc4c3407171e139e221f
star <chlvkfksquf@gmail.com>
2023-07-06 10:48:41 +0900

    WIP

--- src/components/pages/main/Main.style.js
+++ src/components/pages/main/Main.style.js
@@ -2,14 +2,25 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
+  font-family: SlowSlow, sans-serif, Arial;
+  background-color: #426bff;
+  text-align: center;
+  height: 100vh;
+`;
+
+export const TitleContainer = styled.div`
+  padding-top: 100px;
+  margin-bottom: 100px;
+`;
+
+export const SubTitle = styled.div`
+  color: white;
+  font-size: 18px;
 `;
 
 export const Title = styled.div`
-  color: black;
-  font-size: 50px;
-  text-align: center;
-  padding-top: 100px;
-  margin-bottom: 100px;
+  color: white;
+  font-size: 100px;
 `;
 
 export const LoadingImage = styled.img`
@@ -18,8 +29,7 @@ export const LoadingImage = styled.img`
   height: 500px;
 `;
 
-export const Img = styled.img`
-  color: black;
-  width: 100%;
-  height: 500px;
+export const SignupNotice = styled.div`
+  color: white;
+  font-size: 14px;
 `;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Main from 'pages/Main';
-import KakaoMap from 'pages/KakaoMap';
+import Points from 'pages/Points';
 import Feed from 'pages/Feed';
 import FeedDetail from 'pages/FeedDetail';
+import Write from 'pages/Write';
 
 const RoutesContainer = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/points" element={<KakaoMap />} />
+      <Route path="/points" element={<Points />} />
       <Route path="/feed" element={<Feed />} />
       <Route path="/feed/:id" element={<FeedDetail />} />
+      <Route path="/write" element={<Write />} />
     </Routes>
   );
 };
