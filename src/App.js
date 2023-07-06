2f5dd00cd87c1d5af03fed496555fb43b3ead7bc
chxxrin <bewithbomi@gmail.com>
2023-07-06 09:40:44 +0900

    change point

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Routes from 'routes';
 import GlobalStyle from 'styles/reset';
 import Layout from 'pages/Layout';
-import KakaoMap from 'pages/KakaoMap';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Layout router={<Routes />} />
-      <KakaoMap />
     </>
   );
 };
