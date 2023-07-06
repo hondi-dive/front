cd47cd71840fa786270199c61c439cb35437aaa7
star <chlvkfksquf@gmail.com>
2023-07-06 17:19:04 +0900

    Fix nav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { MdNavigateBefore } from "react-icons/md";
+import { MdNavigateBefore } from 'react-icons/md';
 import styled from 'styled-components';
 
 const TopNav = ({ title, actionName, action }) => {
@@ -7,53 +7,64 @@ const TopNav = ({ title, actionName, action }) => {
 
   return (
     <div>
-        <NavbarContainer>
-            <MdNavigateBefore className="icon" size="30" color="black"  onClick={() => {
-              navigate(-1);
-            }}/>
-            <NavbarPosition>{title}</NavbarPosition>  
-            <NavbarAction style={{
+      <NavbarContainer>
+        <MdNavigateBefore
+          className="icon"
+          size="30"
+          color="black"
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
+        <NavbarPosition>{title}</NavbarPosition>
+        <NavbarAction
+          style={{
             fontSize: 18,
             fontWeight: 700,
             color: '#426BFF',
           }}
-          onClick={action} >{actionName}</NavbarAction>
-        </NavbarContainer>
-  </div>
+          onClick={action}
+        >
+          {actionName}
+        </NavbarAction>
+      </NavbarContainer>
+    </div>
   );
 };
 
 export default TopNav;
 
 export const NavbarContainer = styled.div`
-    width: 100%;
-    height: 50px;
-    flex-shrink: 0;
-    background: #FFF;
-    box-shadow: 0px 0px 12px 3px rgba(56, 56, 56, 0.10);
-    display: flex;
-    justify-content: space-around;
-    text-align: center;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right:0;
-    overflow: hidden;
+  width: 100%;
+  height: 50px;
+  flex-shrink: 0;
+  background: #fff;
+  box-shadow: 0px 0px 12px 3px rgba(56, 56, 56, 0.1);
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  max-width: 393px;
+  margin: 0 auto;
 `;
 
 export const NavbarPosition = styled.div`
-color: #000;
-font-family: Pretendard;
-font-size: px;
-font-style: normal;
-font-weight: 500;
-line-height: 18px;
-text-transform: uppercase;
+  color: #000;
+  font-family: Pretendard;
+  font-size: px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+  text-transform: uppercase;
 `;
 
 export const NavbarAction = styled.div`
-fontSize: 18px,
-fontWeight: 700,
-color: '#426BFF'
+  font-size: 18px;
+  font-weight: 700;
+  color: '#426BFF';
 `;
